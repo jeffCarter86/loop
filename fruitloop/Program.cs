0daa559f793a7c05fadeb1158bfb40a800af2879
unknown <jeffrey.r.carter86@gmail.com>
2020-04-24 14:53:18 -0400

    cleaned up code and added notes explainging parts

--- fruitloop/Program.cs
+++ fruitloop/Program.cs
@@ -13,37 +13,37 @@ namespace fruitloop
             int word;
             string WordValue;
             Console.WriteLine("Enter word here: ");
-            WordValue = Console.ReadLine();
+            WordValue = Console.ReadLine(); //accepts user input for a value
 
-            word = (WordValue.Length);
+            word = (WordValue.Length);      /// turns string into a interger
 
            foreach (char c in WordValue)
             {
-                Console.WriteLine(c); //alternate to spell world invidual letters
+                Console.WriteLine(c); //alternate to spell word invidual letters
             }
-            for (int i = 0; i < WordValue.Length; i++)
+            for (int i = 0; i < WordValue.Length; i++) //spell individual letters of word
             {
                 Console.WriteLine(WordValue[i]);
             }
-            for (int i = 0; i < word; i++)
+            for (int i = 0; i < word; i++) // counts each letter and spaces of a set of words or word
             {
                 Console.WriteLine(i);
             }
-            for (int i=1; i<WordValue.Length; i+=2)
+            for (int i=1; i<WordValue.Length; i+=2) // displays every other letter of input
             {
                 Console.WriteLine(WordValue[i]);
             }
 
-            if (word % 3 ==0)
+            if (word % 3 ==0) //sees if a word or phrase is divisible completely by 3
             {
-           for  (int i = 0; i < WordValue.Length; i += 3)
+           for  (int i = 0; i < WordValue.Length; i += 3) //if divisible by 3 prints approppiate letter
                 {
-                    Console.WriteLine(WordValue[i]);
+                    Console.WriteLine(WordValue[i]); 
                 }
             }
             else
             {
-                Console.WriteLine("Not divisible by 3");
+                Console.WriteLine("Not divisible by 3"); //if not does this
             }
 
 
