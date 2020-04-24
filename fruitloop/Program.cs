b22f81831deaabadca47c083550f11f170a0973e
unknown <jeffrey.r.carter86@gmail.com>
2020-04-24 12:33:34 -0400

    seperated letter print out

--- fruitloop/Program.cs
+++ fruitloop/Program.cs
@@ -12,19 +12,30 @@ namespace fruitloop
         {
             int word;
             string WordValue;
-
+            Console.WriteLine("Enter word here: ");
             WordValue = Console.ReadLine();
 
             word = (WordValue.Length);
 
 
-            for (int i = 0; i < word+1; i++)
+            for (int i = 0; i < word; i++)
             {
                 Console.WriteLine(i);
             }
 
+            Console.ReadLine();
 
-            
+            Console.WriteLine("Enter Another word");
+            foreach (char c in WordValue)
+            {
+                Console.WriteLine(c);
+            }
+            for (int i = 0; i < WordValue.Length; i++)
+            {
+                Console.WriteLine(WordValue[i]);
+            }
+
+
 
 
         }
